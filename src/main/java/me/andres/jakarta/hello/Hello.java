6c1ea796523f0b9c3d2b0153bda1fb3ade63a468
programador5 <programador5@cefafa.com.sv>
2024-10-08 15:45:54 -0600

    init

---
+++ src/main/java/me/andres/jakarta/hello/Hello.java
@@ -0,0 +1,14 @@
+package me.andres.jakarta.hello;
+
+public class Hello {
+
+	private String name;
+	
+	public Hello(String name) {
+        this.name = name;
+	}
+
+	public String getHello(){
+		return name;
+	}
+}
